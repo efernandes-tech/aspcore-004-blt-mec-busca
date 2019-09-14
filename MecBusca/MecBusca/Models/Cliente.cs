b5657a9741f882d949ab7dae5280459b61b130e7
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-09-14 19:52:28 -0300

    parte 4: configurando a model

---
+++ MecBusca/MecBusca/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MecBusca.Models
+{
+    public class Cliente
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
+
+
---
+++ MecBusca/MecBusca/Models/DataAccess.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MecBusca.Models
+{
+    public class DataAccess
+    {
+        MongoClient _client;
+        IMongoDatabase _db;
+
+        public DataAccess()
+        {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _db = _client.GetDatabase("aspcore-004-blt-mec-busca");
+        }
+
+        public long CountCustomers()
+        {
+            return _db.GetCollection<Cliente>(typeof(Cliente).Name).Count(new FilterDefinitionBuilder<Cliente>().Empty);
+        }
+    }
+}
